23069551cece0574d2fd129ac7fcb20f11969983
webpawan <pawansaini66477@gmail.com>
2023-03-13 23:10:33 +0530

    server real time function is also completed

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+import { createServer } from "http";
+import { Server } from "socket.io";
 const app = express();
 import "./db/connection.js";
 import userRouter from "./Routes/userRoutes.js";
@@ -9,24 +11,57 @@ import chatRouter from "./Routes/chatRoutes.js";
 import messageRoute from "./Routes/messateRoutes.js";
 import cookieParser from "cookie-parser";
 
-app.get("/",(req,res)=>{
-  res.json("successfull api working")
-})
-
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "*",
+  },
+});
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use("/api/user", userRouter);
-app.use('/api/chat',chatRouter);
-app.use("/api/message",messageRoute)
-// ----------------------------------------------
-// yadi usko koi route ni milti ha to bo in niche bali rout me jayga firstbali ma bhi error ke response ni mila to second bale ko dhkega
-// isko bad me dhkte ha 
-// app.use(notFound)
-// app.use(notFound);
-// --------------------------------------
+app.use("/api/chat", chatRouter);
+app.use("/api/message", messageRoute);
+
+io.on("connection", (socket) => {
+  console.log("A user has connected");
 
 
 
-app.listen(PORT, () => {
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("uiser joined room" + room);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new Message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat users not defiend");
+
+    chat.users.map((user) => {
+      if (user._id === newMessageRecieved.sender._id) {
+        return;
+      }
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup",()=>{
+    console.log("user desconnected");
+    socket.leave(userData._id)
+  })
+});
+
+httpServer.listen(PORT, () => {
   console.log("start server");
 });
