fb87a64ed3537d5d668434882cc5d4e4b0ff8605
webpawan <pawansaini66477@gmail.com>
2023-03-20 22:25:38 +0530

    server proxiy error striil 1.6

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,10 +1,14 @@
 import Express from "express";
-import { allUsers, login, registerUser } from "../controllers/userControllers.js";
+import {
+  allUsers,
+  login,
+  registerUser,
+} from "../controllers/userControllers.js";
 import { verifyToken } from "../middlewares/verifyTokenMiddleware.js";
 
 const router = Express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", login);
-router.get("/",verifyToken,allUsers)
+router.get("/", verifyToken, allUsers);
 export default router;
