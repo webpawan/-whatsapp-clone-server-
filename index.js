a6eac574b4ac916dcc1edb946b168da3a26779d3
webpawan <pawansaini66477@gmail.com>
2023-03-22 19:56:32 +0530

    problem not going

--- index.js
+++ index.js
@@ -10,15 +10,21 @@ import userRouter from "./Routes/userRoutes.js";
 import chatRouter from "./Routes/chatRoutes.js";
 import messageRoute from "./Routes/messateRoutes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://chatAppServer.onrender.com"],
+  })
+);
 
+// const httpServer = createServer(app);
+// const io = new Server(httpServer, {
+//   pingTimeout: 60000,
+//   cors: {
+//     origin:"*",
+//   },
+// });
 
-const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  pingTimeout: 60000,
-  cors: {
-    origin: process.env.BASE_URL,
-  },
-});
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", userRouter);
@@ -26,44 +32,49 @@ app.use("/api/chat", chatRouter);
 app.use("/api/message", messageRoute);
 
 
-io.on("connection", (socket) => {
-  console.log("A user has connected");
+// io.on("connection", (socket) => {
+//   console.log("A user has connected");
 
 
 
-  socket.on("setup", (userData) => {
-    socket.join(userData._id);
-    socket.emit("connected");
-  });
+//   socket.on("setup", (userData) => {
+//     socket.join(userData._id);
+//     socket.emit("connected");
+//   });
 
-  socket.on("join chat", (room) => {
-    socket.join(room);
-    console.log("user joined room" + room);
-  });
+//   socket.on("join chat", (room) => {
+//     socket.join(room);
+//     console.log("user joined room" + room);
+//   });
 
-  socket.on("typing", (room) => socket.in(room).emit("typing"));
-  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+//   socket.on("typing", (room) => socket.in(room).emit("typing"));
+//   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
-  socket.on("new Message", (newMessageRecieved) => {
-    var chat = newMessageRecieved.chat;
+//   socket.on("new Message", (newMessageRecieved) => {
+//     var chat = newMessageRecieved.chat;
 
-    if (!chat.users) return console.log("chat users not defiend");
+//     if (!chat.users) return console.log("chat users not defiend");
 
-    chat.users.map((user) => {
-      if (user._id === newMessageRecieved.sender._id) {
-        return;
-      }
+//     chat.users.map((user) => {
+//       if (user._id === newMessageRecieved.sender._id) {
+//         return;
+//       }
 
-      socket.in(user._id).emit("message recieved", newMessageRecieved);
-    });
-  });
+//       socket.in(user._id).emit("message recieved", newMessageRecieved);
+//     });
+//   });
 
-  socket.off("setup",()=>{
-    console.log("user desconnected");
-    socket.leave(userData._id)
-  })
-});
+//   socket.off("setup",()=>{
+//     console.log("user desconnected");
+//     socket.leave(userData._id)
+//   })
+// });
 
-httpServer.listen(PORT, () => {
+// httpServer.listen(PORT, () => {
+//   console.log("start server");
+// });
+
+
+app.listen(PORT, () => {
   console.log("start server");
 });
