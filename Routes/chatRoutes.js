74cd4b3fb2d32ed7e877fac7bd956a7610eb6a3d
webpawan <pawansaini66477@gmail.com>
2023-03-27 00:18:29 +0530

    just find fetch error i tink this is jwt error01

--- Routes/chatRoutes.js
+++ Routes/chatRoutes.js
@@ -1,14 +1,21 @@
 import express from "express";
-import { accessChat, addGroup, createGroupChat, fetchChats, removeGroup, renameGroup } from "../controllers/chatController.js";
+import {
+  accessChat,
+  addGroup,
+  createGroupChat,
+  fetchChats,
+  removeGroup,
+  renameGroup,
+} from "../controllers/chatController.js";
 import { verifyToken } from "../middlewares/verifyTokenMiddleware.js";
 
 const router = express.Router();
 
-router.post("/",verifyToken,accessChat);
-router.get("/",fetchChats);
-router.post("/group", verifyToken,createGroupChat);
-router.put("/rename", verifyToken,renameGroup);
-router.put("/groupadd", verifyToken,addGroup);
-router.put("/groupremove", verifyToken,removeGroup);
+router.post("/", verifyToken, accessChat);
+router.get("/", verifyToken, fetchChats);
+router.post("/group", verifyToken, createGroupChat);
+router.put("/rename", verifyToken, renameGroup);
+router.put("/groupadd", verifyToken, addGroup);
+router.put("/groupremove", verifyToken, removeGroup);
 
-export default router
+export default router;
